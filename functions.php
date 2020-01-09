86136a6a82ca50318da1a03aacbcde4da29ab12d
henock fantahun <henockfztb@gmail.com>
2020-01-09 11:29:51 +0300

    functions.php edit

--- functions.php
+++ functions.php
@@ -256,3 +256,15 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+function RadTheme_enqueue_styles() {
+
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
+    wp_enqueue_style( 'core', get_template_directory_uri() . '/style.css' );
+  
+  }
+  add_action( 'wp_enqueue_scripts', 'RadTheme_enqueue_styles');
+  
+  function RadTheme_enqueue_scripts() {
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/vendor/bootstrap.bundle.min.js', array( 'jquery' ) );
+  }
+  add_action( 'wp_enqueue_scripts', 'RadTheme_enqueue_scripts');
