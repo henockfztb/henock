4fa009b2698a0717645c495079e1a1e42df47551
henock fantahun <henockfztb@gmail.com>
2020-01-23 19:52:21 +0300

    page.php modification

--- page.php
+++ page.php
@@ -14,6 +14,9 @@
 
 get_header();
 ?>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-8">
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
@@ -35,6 +38,10 @@ get_header();
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php
-get_sidebar();
-get_footer();
+	</div>
+    <div class="col-sm-4">
+    <?php get_sidebar(); ?>
+    </div>
+  </div>
+</div>
+
