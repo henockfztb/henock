30daa2048a6a154d01e59bf075afc716bb15d3d1
henock fantahun <henockfztb@gmail.com>
2020-01-30 12:17:34 +0300

    edit

--- footer.php
+++ footer.php
@@ -46,7 +46,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'henock-fantahun' ), 'henock-fantahun', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'henock-fantahun' ), 'henock-fantahun', '<a href="http://henock.com/">henock</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
