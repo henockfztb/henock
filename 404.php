458b64a365ef7082d5ea269b5002ff02210698a9
henock fantahun <henockfztb@gmail.com>
2020-01-31 00:56:56 +0300

    404 page

--- 404.php
+++ 404.php
@@ -10,45 +10,34 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area" style="text-align: center">
 		<main id="main" class="site-main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
+				
+
+
+				<img src="404 pic.jpg" alt="404 error" height="442" width="342">
+
+
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'henock-fantahun' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'henock-fantahun' ); ?></p>
+
+					
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links on the menu or a search?', 'henock-fantahun' ); ?></p>
 
 					<?php
 					get_search_form();
 
-					the_widget( 'WP_Widget_Recent_Posts' );
+					
 					?>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'henock-fantahun' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
+					
 
-					<?php
-					/* translators: %1$s: smiley */
-					$henock_fantahun_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'henock-fantahun' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$henock_fantahun_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
